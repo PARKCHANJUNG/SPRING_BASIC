b9df53a66b93474f9408b685a07e65bf43f0892f
parkchanjung <park992002@naver.com>
2018-07-20 19:28:00 +0900

    log4j commit

--- src/main/java/com/spring/basic/lombok/ctrl/ComputerCtrl.java
+++ src/main/java/com/spring/basic/lombok/ctrl/ComputerCtrl.java
@@ -10,22 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.basic.lombok.bean.Computer;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 public class ComputerCtrl {
 	
-	private static final Logger log = LoggerFactory.getLogger(ComputerCtrl.class);
+	private static final Logger logger = LoggerFactory.getLogger(ComputerCtrl.class);
 	
 	@GetMapping("getComputer1")
 	public void getComputer1(HttpServletRequest request, HttpServletResponse response) {
 		Computer computer = new  Computer();
-		log.info("TEST");
 		computer.foo();
+		logger.info("Computer1 : {}", "computer1");
 	}
 	
 	@GetMapping("getComputer2")
 	public void getComputer2(HttpServletRequest request, HttpServletResponse response) {
 		Computer computer = new  Computer();
 		computer.foo();
+		logger.info("Computer2 : {}", "computer2");
 	}
 
 }
